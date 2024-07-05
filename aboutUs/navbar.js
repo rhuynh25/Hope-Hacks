e03dc31cf11d9d4534c255b18b9d24660889d9b7
Joshua Drakeford <JD@Administrators-MacBook-Pro.local>
2024-07-05 18:15:42 -0400

    updates to about us page

--- aboutUs/navbar.js
+++ aboutUs/navbar.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
 }
